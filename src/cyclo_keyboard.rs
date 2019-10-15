16de483b1da5bbe2bb0382ac34312b278eab9066
Henning Meyer <tutmann@gmail.com>
2019-10-15 09:11:20 +0100

    Using 8to3 line decoder to drive the matrix.

--- src/cyclo_keyboard.rs
+++ src/cyclo_keyboard.rs
@@ -1,6 +1,6 @@
 use wiringpi::pin::Value::{High, Low};
 
-const W_PINS: [u16; 8] = [0, 1, 2, 3, 4, 5, 6, 7];
+const W_PINS: [u16; 3] = [6, 10, 11];
 const R_PINS: [u16; 8] = [22, 23, 24, 25, 26, 27, 28, 29];
 
 struct KeyboardHardware {
@@ -15,7 +15,7 @@ impl KeyboardHardware {
                 .iter()
                 .map(|p| {
                     let pin = pi.output_pin(*p);
-                    pin.digital_write(High);
+                    pin.digital_write(Low);
                     pin
                 })
                 .collect(),
@@ -41,11 +41,11 @@ impl KeyboardHardware {
     }
 
     pub fn get_current_keys(&self) -> u64 {
-        self.w_pins.iter().fold(0, |acc, x| {
-            x.digital_write(Low);
-            let result = acc << 8 | self.read_pins() as u64;
-            x.digital_write(High);
-            result
+        (0..8).fold(0, |acc, x| {
+            for (bit, pin) in self.w_pins.iter().enumerate() {
+                pin.digital_write(if (x & (1 << bit)) != 0 { High } else { Low });
+            }
+            acc << 8 | self.read_pins() as u64
         })
     }
 }
@@ -71,68 +71,68 @@ pub enum Key {
 impl Key {
     fn from_keycode(code: u8) -> Key {
         match code {
-            49 => Key::Code,
-            0 => Key::Char('1'),
-            3 => Key::Char('2'),
-            4 => Key::Char('3'),
-            5 => Key::Char('4'),
-            1 => Key::Char('5'),
-            2 => Key::Char('6'),
-            7 => Key::Char('7'),
-            6 => Key::Char('8'),
-            34 => Key::Char('9'),
-            33 => Key::Char('0'),
-            42 => Key::Char('ß'),
-            41 => Key::Char('`'),
-            54 => Key::Back,
-            55 => Key::Del,
+            9 => Key::Code,
+            56 => Key::Char('1'),
+            59 => Key::Char('2'),
+            60 => Key::Char('3'),
+            61 => Key::Char('4'),
+            57 => Key::Char('5'),
+            58 => Key::Char('6'),
+            63 => Key::Char('7'),
+            62 => Key::Char('8'),
+            26 => Key::Char('9'),
+            25 => Key::Char('0'),
+            18 => Key::Char('ß'),
+            17 => Key::Char('`'),
+            14 => Key::Back,
+            15 => Key::Del,
 
-            50 => Key::Tab,
-            8 => Key::Char('q'),
-            11 => Key::Char('w'),
-            12 => Key::Char('e'),
-            13 => Key::Char('r'),
-            9 => Key::Char('t'),
-            10 => Key::Char('z'),
-            15 => Key::Char('u'),
-            14 => Key::Char('i'),
-            39 => Key::Char('o'),
-            37 => Key::Char('p'),
-            46 => Key::Char('ü'),
-            47 => Key::Char('+'),
-            52 => Key::Return,
-            45 => Key::T,
+            10 => Key::Tab,
+            48 => Key::Char('q'),
+            51 => Key::Char('w'),
+            52 => Key::Char('e'),
+            53 => Key::Char('r'),
+            49 => Key::Char('t'),
+            50 => Key::Char('z'),
+            55 => Key::Char('u'),
+            54 => Key::Char('i'),
+            31 => Key::Char('o'),
+            29 => Key::Char('p'),
+            22 => Key::Char('ü'),
+            23 => Key::Char('+'),
+            12 => Key::Return,
+            21 => Key::T,
 
-            53 => Key::CapsLock,
-            16 => Key::Char('a'),
-            19 => Key::Char('s'),
-            20 => Key::Char('d'),
-            21 => Key::Char('f'),
-            17 => Key::Char('g'),
-            18 => Key::Char('h'),
-            23 => Key::Char('j'),
-            22 => Key::Char('k'),
-            38 => Key::Char('l'),
-            36 => Key::Char('ö'),
-            44 => Key::Char('ä'),
-            43 => Key::Down,
+            13 => Key::CapsLock,
+            40 => Key::Char('a'),
+            43 => Key::Char('s'),
+            44 => Key::Char('d'),
+            45 => Key::Char('f'),
+            41 => Key::Char('g'),
+            42 => Key::Char('h'),
+            47 => Key::Char('j'),
+            46 => Key::Char('k'),
+            30 => Key::Char('l'),
+            28 => Key::Char('ö'),
+            20 => Key::Char('ä'),
+            19 => Key::Down,
 
-            56 => Key::ShiftLeft,
-            24 => Key::Char('y'),
-            27 => Key::Char('x'),
-            28 => Key::Char('c'),
-            29 => Key::Char('v'),
-            25 => Key::Char('b'),
-            26 => Key::Char('n'),
-            31 => Key::Char('m'),
-            30 => Key::Char(','),
-            35 => Key::Char('.'),
-            32 => Key::Char('-'),
-            59 => Key::ShiftRight,
+            0 => Key::ShiftLeft,
+            32 => Key::Char('y'),
+            35 => Key::Char('x'),
+            36 => Key::Char('c'),
+            37 => Key::Char('v'),
+            33 => Key::Char('b'),
+            34 => Key::Char('n'),
+            39 => Key::Char('m'),
+            38 => Key::Char(','),
+            27 => Key::Char('.'),
+            24 => Key::Char('-'),
+            3 => Key::ShiftRight,
 
-            48 => Key::Mod,
-            51 => Key::Char(' '),
-            40 => Key::WeirdE,
+            8 => Key::Mod,
+            11 => Key::Char(' '),
+            16 => Key::WeirdE,
 
             x => Key::Unknown(x),
         }
